a3d391d64aa8d6eaf97937640c666d6d7802ffba
Gorgi16 <gorgialvarado212009@gmail.com>
2025-05-12 13:30:41 -0600

    Continuacion de CotidianoEmpleados

---
+++ Seccion_7/COTIDIANO EMPLEADO/misScript.js
@@ -0,0 +1,21 @@
+function Empleado(){
+    this.Experiencia = Programador;
+    this.Nombre = Miguel;
+    this.Apellido= Rojas;
+    this.Nacimiento= 1990;
+    this.Cargo = Programador;
+    };
+function agregarEmpleados(){
+let Jonny = new Empleado1(); 
+let Natalie = new Empleado2(); 
+let Ronaldo = new Empleado3(); 
+}
+let miArray = [Empleado];
+for(Empleado in miArray)
+{console.log(Empleado + " - " + miArray[propiedad])}
+
+Empleado1.Cargo = 'Programador';
+Empleado1['---'] = '---';
+Empleado1.agregarEmpleados = function() {
+    alert("---");
+};
